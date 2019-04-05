991400d6c8bba01d02bacd0c4e4e1130b00ce7b8
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2019-04-05 19:15:13 +0530

    Pull request successfuly closed

--- web/themes/custom/stabletheme/gulpfile.js
+++ web/themes/custom/stabletheme/gulpfile.js
@@ -1,3 +1,24 @@
+const sass = require('gulp-sass');
+
+var styleSRC = 'app/scss/styles.scss'
+var styleDIST = './dist/css/'
+
+function css(done){
+    gulp.src( styleSRC )
+        .pipe( sourcemaps.init())
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest( styleDIST ))
+        .pipe( browserSync.stream() )
+    done()
+};
 
 
-
+gulp.task("css", css);
