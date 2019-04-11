9c31a75600f5c0fb25305116b5abaa51f3fecb66
meenabisht <meena.bisht392@gmail.com>
2019-04-11 11:29:22 +0530

    git changes

---
+++ web/themes/custom/stabletheme/js/slider.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+// $('.para-slide').slick();
+// });
+
+(function ($, Drupal){
+    Drupal.behaviors.demoblock = {
+        attach: function (context, settings) {
+
+            jQuery(document).ready(function($) {
+                // Code that uses jQuery's $ can follow here.
+                console.log('hello');
+                $('.para-slide').slick();
+            });
+        }
+      };
+    })(jQuery, Drupal);
