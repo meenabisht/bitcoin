5f1ba0721392afddce2f8e2425326fa996c943a4
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2019-04-15 18:57:58 +0530

    Icons paragraph partially completed and contents for Service part has been created

--- web/themes/custom/stabletheme/gulpfile.js
+++ web/themes/custom/stabletheme/gulpfile.js
@@ -1,23 +1,27 @@
+const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+var sassGlob = require('gulp-sass-glob');
+var sourcemaps = require('gulp-sourcemaps');
 
-var styleSRC = 'app/scss/styles.scss'
+var styleSRC = './scss/styles.scss'
 var styleDIST = './dist/css/'
 
 function css(done){
-    gulp.src( styleSRC )
-        .pipe( sourcemaps.init())
-        .pipe(sass({
-            errorLogToConsole: true,
-            outputStyle: 'compressed'
-        }))
-        .on('error', console.error.bind(console))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false}))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest( styleDIST ))
-        .pipe( browserSync.stream() )
-    done()
+   gulp.src( styleSRC )
+       .pipe(sassGlob())
+       .pipe( sourcemaps.init())
+       .pipe(sass({
+           errorLogToConsole: true,
+           outputStyle: 'compressed'
+       }))
+       .on('error', console.error.bind(console))
+       .pipe(autoprefixer({
+           browsers: ['last 2 versions'],
+           cascade: false}))
+       .pipe(sourcemaps.write('./'))
+       .pipe(gulp.dest( styleDIST ))
+   done()
 };
 
 
