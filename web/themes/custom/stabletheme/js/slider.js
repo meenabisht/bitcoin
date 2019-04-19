04f0ecc804b19ac69bc02df027109239dbb958f3
meenabisht <meena.bisht392@gmail.com>
2019-04-19 20:37:55 +0530

    copyright,testimonial,block

--- web/themes/custom/stabletheme/js/slider.js
+++ web/themes/custom/stabletheme/js/slider.js
@@ -7,34 +7,27 @@
                 console.log('hello');
                 $('.para-slide').slick({
                         infinite: false,
-<<<<<<< HEAD
-=======
-
->>>>>>> b0c28013a64fefdbb003ff0dcf6a499a78991bab
                         dots: true,
                         arrows: false,
                         fade: true,
                         cssEase: 'linear',
                         appendDots:$(".para-slide"),
-<<<<<<< HEAD
                 });                
             });
 
             jQuery(document).ready(function($) {
                 // Code that uses jQuery's $ can follow here.
-                $('.views-row').slick({
+                $('#block-views-block-testimonial-view-block-1 .views-view-grid .views-row').slick({
                     infinite: false,
                     dots: true,
                     arrows: false,
-                    fade: true,
+                    fade: false,
+                    speed: 300,
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     cssEase: 'linear',
-                    appendDots:$(".views-row"),
                 });                
-=======
-                });
->>>>>>> b0c28013a64fefdbb003ff0dcf6a499a78991bab
+                
             });
         }
       };
